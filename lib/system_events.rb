b9de3872d88c0fb574859881032f005fdfea1abe
Tomáš Dundáček <tomas@dundacek.cz>
2022-02-02 13:59:14 +0100

    Initial commit

---
+++ lib/system_events.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "system_events/version"
+
+module SystemEvents
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/system_events_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SystemEvents do
+  it "has a version number" do
+    expect(SystemEvents::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
