7e401cdf740418c533974d2076ab4a5a69365a05
Tomáš Dundáček <tomas@dundacek.cz>
2022-03-03 08:45:57 +0100

    Add AMQP adapter

---
+++ lib/system_events/adapters/amqp_adapter.rb
@@ -0,0 +1,41 @@
+module SystemEvents
+  module Adapters
+    class AmqpAdapter
+
+      def initialize(dsn: ENV['AMQP_URL'], exchange_name: :events)
+        @dsn = dsn
+        @exchange_name = exchange_name
+      end
+
+      def handle_event(event_name, *args, **kwargs)
+        message = {
+          event_name: event_name,
+          args:       args,
+          kwargs:     kwargs
+        }.to_json
+
+        exchange.publish(message)
+      end
+
+      def handles_event?(event_name)
+        true
+      end
+
+      private
+
+      attr_accessor :dsn, :exchange_name
+
+      def exchange
+        @exchange ||= channel.fanout(exchange_name.to_s, durable: true)
+      end
+
+      def channel
+        @channel ||= connection.create_channel
+      end
+
+      def connection
+        @connection ||= Bunny.new(dsn).tap(&:start)
+      end
+    end
+  end
+end
