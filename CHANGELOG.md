4894bf64d09329d0288a8ddd8b4a1cbec3dd7539
Tomáš Dundáček <tomas@dundacek.cz>
2023-01-15 08:50:10 +0100

    Update Sidekiq adapter to be Sidekiq 7 compatible

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ## [Unreleased]
 
+## [0.0.3] - 2023-01-15
+* Update Sidekiq Adapter to be Sidekiq 7 compatible
 
 ## [0.0.2] - 2022-05-05
 * Use RABBITMQ_URL instead of AMQP_URL ENV variable
--- lib/phantom_events/adapters/sidekiq_adapter.rb
+++ lib/phantom_events/adapters/sidekiq_adapter.rb
@@ -5,7 +5,6 @@ module PhantomEvents
       def initialize(listeners_path:,
                      default_queue: :default)
         @listeners_path = listeners_path
-        @parent_class = parent_class
         @default_queue = default_queue
 
         setup_adapter_job_class!
@@ -13,7 +12,10 @@ module PhantomEvents
 
       def handle_event(event_name, *args, **kwargs)
         listeners_for_event(event_name).each do |listener_klass|
-          AdapterJob.perform_async(listener_klass, event_name, *args + kwargs)
+          args.each do |arg|
+            arg.stringify_keys! if arg.is_a?(Hash)
+          end
+          AdapterJob.perform_async(listener_klass.to_s, event_name.to_s, *args, kwargs.to_hash)
         end
       end
 
@@ -23,7 +25,7 @@ module PhantomEvents
 
       private
 
-      attr_reader :listeners_path, :parent_class, :default_queue
+      attr_reader :listeners_path, :default_queue
 
       def listeners
         listeners_path.glob("**/*.rb").map do |pathname|
@@ -42,8 +44,10 @@ module PhantomEvents
         klass = Class.new do
           include Sidekiq::Job
 
-          def perform(klass, event_name, *args)
-            kwargs = args.pop.symbolize_keys! if args.last.is_a?(Hash)
+          def perform(klass, event_name, *args, kwargs)
+            args.each do |arg|
+              arg.with_indifferent_access if arg.is_a?(Hash)
+            end
             klass.safe_constantize.new.public_send(event_name, *args, **kwargs)
           end
         end
